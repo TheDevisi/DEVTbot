033286b07d7badba8b792f227657dad0c26c00de
TheDevisi <syntax_samp@proton.me>
2024-05-05 13:47:38 +0300

    Not bot working with slash commands instead of "!"

--- DEVTBot.py
+++ DEVTBot.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 import json
 import sys
+import pycord
 
 #import asyncio
 #TODO Asyncio doesen't using, i'll rewrite code with it in the future to make it more safe
@@ -31,18 +32,19 @@ async def on_message(message):
         return
     await bot.process_commands(message)
 
-    print(f"{bot.user.name} got message : {message.content}, on server {message.guild}")
-    # TODO should return some info, but it doesent. i dont know why.
+
 
 
 
 #Get user's PFP
-@bot.command()
+@bot.slash_command(name='get_profile_picture', description='You will get your profile picture')
 async def get_avatar(ctx, member: discord.Member = None):
     if not member:
         member = ctx.message.author
     userAvatar = member.avatar.url
-    await ctx.send(f"{userAvatar}\n Here is you profile picture!")
+    await ctx.send(f"{userAvatar}\n Here is profile picture!")
+
+#TODO Fix a "Activity not found. Start a new one?", remove link to pfp in message (for ex: https://cdn.discordapp.com/avatars/..)
 
 
 bot.run(TOKEN)
